400b2d370f6e5fe3609707d66aeadae3098d4df2
Matthew Hoekstra <matthew.e.hoekstra@gmail.com>
2024-01-01 01:25:56 +0000

    fixed forgotten reference to llama_utils

--- reset_RAG_database.py
+++ reset_RAG_database.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-import llama_utils
+import transformers_utils
 import chroma_utils
 import logging
 
-logging.getLogger('llama2_streamlit').setLevel(logging.DEBUG)
+logging.getLogger('transformers_streamlit').setLevel(logging.DEBUG)
 
-config = llama_utils.read_config()
+config = transformers_utils.read_config()
 
 #This script always destroys and rebuilds the DB
 #regardless of the rest of the contents of the config file
